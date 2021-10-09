d33305302d40dbd61a1964aab4461dda91fc1b83
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2021-10-09 12:00:18 -0300

    Update First_game.py

--- First_game.py
+++ First_game.py
@@ -3,6 +3,25 @@ from pygame.locals import *
 from sys import exit
 from random import randint
 
+
+def bigger_snake(snake_body_growth):
+    for position in snake_body_growth:
+        pygame.draw.rect(screen, (0, 255, 0), (position[0], position[1], 20, 20))
+
+
+def restart_game():
+    global points, initial_compriment, snake_x, snake_y, snake_body_growth, snake_head_growth, red_apple_x, red_apple_y, game_over
+    points = 0
+    initial_compriment = 5
+    snake_x = int(width/2 - 20)
+    snake_y = int(length/2 - 25)
+    snake_body_growth = []
+    snake_head_growth = []
+    red_apple_x = randint(40, 600)
+    red_apple_y = randint(50, 430)
+    game_over = False
+
+
 pygame.init()
 
 pygame.mixer.music.set_volume(0.4)
@@ -14,8 +33,15 @@ collision_sound = pygame.mixer.Sound('sound_effect.wav')
 width = 640
 length = 480
 
-x_blue = randint(40, 600)
-y_blue = randint(50, 430)
+snake_x = int(width/2 - 20)
+snake_y = int(length/2 - 25)
+
+velocity = 10
+x_control = velocity
+y_control = 0
+
+red_apple_x = randint(40, 600)
+red_apple_y = randint(50, 430)
 
 points = 0
 font = pygame.font.SysFont('arial', 40, True, True)
@@ -23,44 +49,101 @@ font = pygame.font.SysFont('arial', 40, True, True)
 screen = pygame.display.set_mode((width, length))
 pygame.display.set_caption('Very nice game')
 game_clock = pygame.time.Clock()
-x = int(width/2 - 20)
-y = int(length/2 - 25)
+snake_body_growth = []
+initial_compriment = 5
+game_over = False
 
 while True:
     game_clock.tick(30)
-    screen.fill((0, 0, 0))
-    if y > length:
-        y = 0
-    if y < 0:
-        y = length
-    if x >= width:
-        x = 0
-    if x < 0:
-        x = width
+    screen.fill((255, 255, 255))
+
     message = f'Points: {points}'
-    text_format = font.render(message, True, (255, 255, 255))
+    text_format = font.render(message, True, (0, 0, 0))
+
+    if snake_y > length:
+        snake_y = 0
+    if snake_y < 0:
+        snake_y = length
+    if snake_x >= width:
+        snake_x = 0
+    if snake_x < 0:
+        snake_x = width
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         
-        
-    if pygame.key.get_pressed()[K_a]:
-        x -= 20
-    if pygame.key.get_pressed()[K_s]:
-        y += 20
-    if pygame.key.get_pressed()[K_w]:
-        y -= 20
-    if pygame.key.get_pressed()[K_d]:
-        x += 20
-    red_rectangle = pygame.draw.rect(screen, (200, 50, 50), (x, y, 40, 50))
-    blue_circle = pygame.draw.circle(screen, (0, 0, 200), (x_blue, y_blue), 30)
-
-    if red_rectangle.colliderect(blue_circle):
-        collision_sound.play()
-        x_blue = randint(40, 600)
-        y_blue = randint(50, 430)
-        points += 1
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_control == velocity:
+                    pass
+                else:
+                    x_control = -velocity
+                    y_control = 0
+            if event.key == K_d:
+                if x_control == -velocity:
+                    pass
+                else:
+                    x_control = velocity
+                    y_control = 0
+            if event.key == K_w:
+                if y_control == velocity:
+                    pass
+                else:
+                    y_control = -velocity
+                    x_control = 0
+            if event.key == K_s:
+                if y_control == -velocity:
+                    pass
+                else:
+                    y_control = velocity
+                    x_control = 0
     
+    snake_x += x_control
+    snake_y += y_control
+
+    snake = pygame.draw.rect(screen, (0, 255, 0), (snake_x, snake_y, 20, 20))
+    apple = pygame.draw.circle(screen, (255, 0, 0), (red_apple_x, red_apple_y), 20)
+
+    if snake.colliderect(apple):
+        collision_sound.play()
+        red_apple_x = randint(40, 600)
+        red_apple_y = randint(50, 430)
+        points += 1
+        initial_compriment += 1
+
+    snake_head_growth = []
+    snake_head_growth.append(snake_x)
+    snake_head_growth.append(snake_y)
+
+    snake_body_growth.append(snake_head_growth)
+
+    if snake_body_growth.count(snake_head_growth) > 1:
+        font2 = pygame.font.SysFont("arial", 20, True)
+        message = "Game over! Press 'r' to restart"
+        text_format = font2.render(message, True, (0, 0, 0))
+        ret_text = text_format.get_rect()
+
+        game_over = True
+        while game_over:
+            screen.fill((250, 250, 250))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        restart_game()
+
+            ret_text.center = (width//2, length//2)
+            screen.blit(text_format, ret_text)
+            pygame.display.update()
+
+    if len(snake_body_growth) > initial_compriment:
+        del snake_body_growth[0]
+
+    bigger_snake(snake_body_growth)
+
     screen.blit(text_format, (420, 10))
     pygame.display.update()
